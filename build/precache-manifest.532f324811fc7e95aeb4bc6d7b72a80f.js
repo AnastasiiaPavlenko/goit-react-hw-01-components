2ecbbb5e65cea904f8829278b2ec20c7c922d8c9
Anastasiia Pavlenko <33912901+AnastasiiaPavlenko@users.noreply.github.com>
2019-10-19 19:03:20 +0300

    Add files via upload

---
+++ build/precache-manifest.532f324811fc7e95aeb4bc6d7b72a80f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a408f9c93ec70458dfcc1e4f14a9f31e",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "dfe8a37d9790d28e4916",
+    "url": "/goit-react-hw-01-components/static/css/main.27e37b2d.chunk.css"
+  },
+  {
+    "revision": "076b0858033d45d997dd",
+    "url": "/goit-react-hw-01-components/static/js/2.ea5e6eb5.chunk.js"
+  },
+  {
+    "revision": "dfe8a37d9790d28e4916",
+    "url": "/goit-react-hw-01-components/static/js/main.33101206.chunk.js"
+  },
+  {
+    "revision": "4acb19bdd75534e6f942",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.0511350e.js"
+  }
+]);
